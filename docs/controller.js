cd40fa9956c96a0eafff0908949e40f7f0904b1f
Brian W Bush <consult@brianwbush.info>
2023-02-12 14:13:15 -0700

    Added first swap contract on `mainnet`.

--- docs/controller.js
+++ docs/controller.js
@@ -1,6 +1,15 @@
+
 function openLink(path) {
   if (window.location.origin == "file://")
     window.open("../" + path, 'rwm');
   else
     window.open("https://github.com/bwbush/real-world-marlowe/blob/main/" + path, 'rwm');
+}
+
+function setImage(img, path) {
+  img.onload = ""
+  if (window.location.origin == "file://")
+    img.src = "../" + path
+  else
+    img.src = "https://github.com/bwbush/real-world-marlowe/blob/main/" + path
 }
