c321748c1daa59c1aac19f2be018267a10f44350
Brian W Bush <brian.bush@iohk.io>
2023-08-05 05:03:40 -0600

    PLT-6977 Nix flake for static builds of raffle executables.

---
+++ archives/raffle/nix/iogx-config.nix
@@ -0,0 +1,8 @@
+# This file is part of the IOGX template and is documented at the link below:
+# https://www.github.com/input-output-hk/iogx#32-nixiogx-confignix
+
+{
+  systems = [ "x86_64-linux" ];
+  haskellCompilers = [ "ghc8107" ];
+  shouldCrossCompile = false;
+}
